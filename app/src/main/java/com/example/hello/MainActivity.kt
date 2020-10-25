31771d408fe58109369f173678731742bfe94bbf
tonightrain <805560812@qq.com>
2020-10-25 20:51:21 +0800

    03-constraintLayout

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.hello
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val constraintButton = findViewById<Button>(R.id.button1)
+        constraintButton.setOnClickListener {
+            val intent = Intent()
+            intent.setClass(this, ConstraintActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
