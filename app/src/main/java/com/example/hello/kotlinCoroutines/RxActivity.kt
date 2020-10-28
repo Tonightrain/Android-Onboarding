f44a2ec4212dbb69797e9dc513298d52b0cc4553
tonightrain <805560812@qq.com>
2020-10-28 20:01:06 +0800

    重构06-1

--- app/src/main/java/com/example/hello/kotlinCoroutines/RxActivity.kt
+++ app/src/main/java/com/example/hello/kotlinCoroutines/RxActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hello.R
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -15,30 +16,45 @@ class RxActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rx2)
+//
+//        val buttonTiming = findViewById<Button>(R.id.button_rx)
+//        buttonTiming.setOnClickListener {
+//            var count = 0
+//            GlobalScope.launch {
+//                repeat(11) {
+//                    if (count == 10) {
+//                        setIsEnabled(buttonTiming,true)
+//                    }else{
+//                        setIsEnabled(buttonTiming,false)
+//                    }
+//                    setText(buttonTiming, "The number is $count")
+//                    count++
+//                    delay(1000)
+//                }
+//            }
+//        }
+//    }
+//
+//    private fun setText(text: TextView, value: String) {
+//        runOnUiThread { text.text = value }
+//    }
+//
+//    private fun setIsEnabled(text: TextView,isButtonEnable:Boolean){
+//        runOnUiThread { text.isEnabled = isButtonEnable }
+//    }
 
         val buttonTiming = findViewById<Button>(R.id.button_rx)
         buttonTiming.setOnClickListener {
             var count = 0
-            GlobalScope.launch {
+            GlobalScope.launch(Dispatchers.Main) {
                 repeat(11) {
-                    if (count == 10) {
-                        setIsEnabled(buttonTiming,true)
-                    }else{
-                        setIsEnabled(buttonTiming,false)
-                    }
-                    setText(buttonTiming, "The number is $count")
+                    buttonTiming.isEnabled = count == 10
+                    buttonTiming.text = "The number is $count"
                     count++
                     delay(1000)
                 }
             }
         }
     }
-
-    private fun setText(text: TextView, value: String) {
-        runOnUiThread { text.text = value }
-    }
-
-    private fun setIsEnabled(text: TextView,isButtonEnable:Boolean){
-        runOnUiThread { text.isEnabled = isButtonEnable }
-    }
 }
+
